bc774262ab3be6096435303eda51f2c706fe8750
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2022-03-02 11:17:38 +0300

    BreadcrumbItem object added to use in breadcrumb builder.

---
+++ AvvaMobile.Core/AvvaMobile.Core/BreadcrumbItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AvvaMobile.Core
+{
+    public class BreadcrumbItem
+    {
+        public BreadcrumbItem(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
