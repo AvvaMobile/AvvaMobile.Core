c40217740283db555bb7ed2f4dadea91294eec2c
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2022-03-21 16:42:03 +0300

    ServiceReponse modeli eklendi.

---
+++ AvvaMobile.Core/AvvaMobile.Core/Business/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AvvaMobile.Core.Business
+{
+    public class ServiceResponse
+    {
+        public bool IsSuccess { get; set; } = true;
+        public string? Message { get; set; } = null;
+        public dynamic? Data { get; set; } = null;
+    }
+}
---
+++ AvvaMobile.Core/AvvaMobile.Core/ConstantStrings.cs
@@ -0,0 +1,12 @@
+﻿namespace AvvaMobile.Core
+{
+    public static class ViewDataNames
+    {
+        public static readonly string Title = "Title";
+
+        public static readonly string AsideMenu = "AsideMenu";
+
+        public static readonly string AsideSubMenu = "AsideSubMenu";
+        public static readonly string Breadcrumb = "Breadcrumb";
+    }
+}
