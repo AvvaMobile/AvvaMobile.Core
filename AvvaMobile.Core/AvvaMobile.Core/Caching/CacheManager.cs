23bd208f7b4687f14c7ac1761642e98edefd45ed
Öcal Esmer <ocal@avvamobile.com>
2022-08-25 15:30:41 +0300

    appsettingskeys içinden JWT kaldırıldı.

--- AvvaMobile.Core/AvvaMobile.Core/Caching/CacheManager.cs
+++ AvvaMobile.Core/AvvaMobile.Core/Caching/CacheManager.cs
@@ -86,11 +86,5 @@ namespace AvvaMobile.Core.Caching
         public string CDN_BaseUrl { get { return _cacheManager.Get("CDN_BaseUrl"); } }
         public string CDN_UsersImageFolder { get { return _cacheManager.Get("CDN_UsersImageFolder"); } }
         public string CDN_CustomersImageFolder { get { return _cacheManager.Get("CDN_CustomersImageFolder"); } }
-
-        public string JWT_TokenIssuer { get { return _cacheManager.Get("JWT_TokenIssuer"); } }
-        public string JWT_TokenAudience { get { return _cacheManager.Get("JWT_TokenAudience"); } }
-        public string JWT_TokenSecurityKey { get { return _cacheManager.Get("JWT_TokenSecurityKey"); } }
-
-
     }
 }
