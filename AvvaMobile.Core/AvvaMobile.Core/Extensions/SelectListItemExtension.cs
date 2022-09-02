cff9ecae7a9d8f30aa41954558fd803ec88cb258
Öcal Esmer <ocal@avvamobile.com>
2022-09-02 13:58:29 +0300

    1.0.41 SelectListItemExtension update

--- AvvaMobile.Core/AvvaMobile.Core/Extensions/SelectListItemExtension.cs
+++ AvvaMobile.Core/AvvaMobile.Core/Extensions/SelectListItemExtension.cs
@@ -4,15 +4,15 @@ namespace AvvaMobile.Core.Extensions
 {
     public static class SelectListItemExtension
     {
-        public static List<SelectListItem> AddALLOption(this List<SelectListItem> list)
+        public static List<SelectListItem> AddALLOption(this List<SelectListItem> list, string text)
         {
-            list.Insert(0, new SelectListItem { Text = "-- Tümü --", Value = "-1" });
+            list.Insert(0, new SelectListItem { Text = text, Value = "-1" });
             return list;
         }
 
-        public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list)
+        public static List<SelectListItem> AddSELECTOption(this List<SelectListItem> list, string text)
         {
-            list.Insert(0, new SelectListItem { Text = "-- Seçiniz --", Value = string.Empty });
+            list.Insert(0, new SelectListItem { Text = text, Value = string.Empty });
             return list;
         }
     }
