d3e43df65be60608d7552619a9352948bd1bc9aa
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2022-10-16 15:55:32 +0300

    Parasut project moved to another repo.

--- AvvaMobile.Core/AvvaMobile.Core.Parasut/Models/Common/Relationships.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AvvaMobile.Core.Parasut.Models.Common
-{
-    public class Relationships
-    {
-        public Envelope<ContactBase> contact { get; set; } = new Envelope<ContactBase>();
-    }
-}
