64d3a70629bb31e7b3076b8bbd065208149ad33e
Öcal Esmer <ocal@avvamobile.com>
2023-01-30 16:33:41 +0300

    1.0.74 PushNotificationService Async olmayan metotlar eklendi.

--- AvvaMobile.Core/AvvaMobile.Core/Utilities/PushNotification/PushNotificationService.cs
+++ AvvaMobile.Core/AvvaMobile.Core/Utilities/PushNotification/PushNotificationService.cs
@@ -40,6 +40,35 @@ namespace AvvaMobile.Core.Utilities.PushNotification
             var pusNotificationResult = await SendNotificationAsync(appID, apiKey, userPushIDs, title, message, data);
             return pusNotificationResult;
         }
+
+
+
+        public PushNotificationSendResult Send(string userPushID, string message)
+        {
+            return Send(new List<string> { userPushID }, null, message);
+        }
+        public PushNotificationSendResult Send(List<string> userPushIDs, string message)
+        {
+            return Send(userPushIDs, null, message);
+        }
+
+        public PushNotificationSendResult Send(string userPushID, string title, string message)
+        {
+            return Send(new List<string> { userPushID }, title, message);
+        }
+
+        public PushNotificationSendResult Send(string userPushID, string title, string message, dynamic data)
+        {
+            return SendAsync(new List<string> { userPushID }, title, message, data);
+        }
+
+        public PushNotificationSendResult Send(List<string> userPushIDs, string title, string message, dynamic data = null)
+        {
+            string appID = _appSettingsKeys.OneSignal_AppID;
+            string apiKey = _appSettingsKeys.OneSignal_APIKey;
+            var pusNotificationResult = SendNotification(appID, apiKey, userPushIDs, title, message, data);
+            return pusNotificationResult;
+        }
     }
 
     public class BasePushNotificationService
@@ -100,6 +129,62 @@ namespace AvvaMobile.Core.Utilities.PushNotification
                 return result;
             }
         }
+        public PushNotificationSendResult SendNotification(string appID, string apiKey, List<string> userPushIDs, string title, string message, dynamic data = null)
+        {
+            var result = new PushNotificationSendResult();
+
+            if (userPushIDs.Count.Equals(0))
+            {
+                result.IsSuccess = false;
+                result.Message = "Gönderilecek kullanıcı bulunamadı.";
+                return result;
+            }
+
+            string[] customersString = userPushIDs.ToArray();
+
+            var model = new
+            {
+                app_id = appID,
+                include_player_ids = customersString,
+                contents = new { en = message },
+                data = data,
+                headings = new { en = title },
+                ios_badgeType = "Increase",
+                ios_badgeCount = 1
+            };
+
+            try
+            {
+
+                HttpClient client = new();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var serviceUrl = "https://onesignal.com/api/v1/notifications";
+                var requestBody = JsonConvert.SerializeObject(model);
+
+                var request = new HttpRequestMessage();
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(serviceUrl);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("authorization", "Basic " + apiKey);
+
+                var response = client.Send(request);
+
+                var responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                result.ResponseContents = responseBody;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Message = "Bildirim gönderilirken bir hata oluştu. Lütfen hata detayına bakınız. Exception: " + ex.Message;
+                result.Exception = ex;
+                return result;
+            }
+        }
     }
 
     public class PushNotificationSendResult
@@ -117,5 +202,11 @@ namespace AvvaMobile.Core.Utilities.PushNotification
         Task<PushNotificationSendResult> SendAsync(List<string> userPushIDs, string title, string message, dynamic data = null);
         Task<PushNotificationSendResult> SendAsync(string userPushID, string message);
         Task<PushNotificationSendResult> SendAsync(string userPushID, string title, string message, dynamic data = null);
+
+
+        PushNotificationSendResult Send(List<string> userPushIDs, string message);
+        PushNotificationSendResult Send(List<string> userPushIDs, string title, string message, dynamic data = null);
+        PushNotificationSendResult Send(string userPushID, string message);
+        PushNotificationSendResult Send(string userPushID, string title, string message, dynamic data = null);
     }
 }
