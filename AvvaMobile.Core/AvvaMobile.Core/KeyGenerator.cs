230dc2501dd3f721db4e266967aab89cc781e6b7
Murat Yılmaz <murat.yilmaz@avvamobile.com>
2023-09-21 20:21:52 +0300

    Extensions nuGet updated.

--- AvvaMobile.Core/AvvaMobile.Core/KeyGenerator.cs
+++ AvvaMobile.Core/AvvaMobile.Core/KeyGenerator.cs
@@ -1,39 +1,38 @@
-﻿namespace AvvaMobile.Core
+﻿namespace AvvaMobile.Core;
+
+public class KeyGenerator
 {
-    public class KeyGenerator
+    public static string CreateRandomPassword(int length = 14)
     {
-        public static string CreateRandomPassword(int length = 14)
+
+        var validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+        var random = new Random();
+
+        var chars = new char[length];
+        for (var i = 0; i < length; i++)
         {
-
-            var validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
-            var random = new Random();
-
-            var chars = new char[length];
-            for (var i = 0; i < length; i++)
-            {
-                chars[i] = validChars[random.Next(0, validChars.Length)];
-            }
-            return new string(chars);
+            chars[i] = validChars[random.Next(0, validChars.Length)];
         }
+        return new string(chars);
+    }
 
-        public static int CreateRandomNumber(int min = 100000, int max = 999999)
+    public static int CreateRandomNumber(int min = 100000, int max = 999999)
+    {
+        var random = new Random();
+        return random.Next(min, max);
+    }
+
+    public static string CreateAlphanumericCode(int length = 14)
+    {
+
+        var validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+
+        var chars = new char[length];
+        for (var i = 0; i < length; i++)
         {
-            var random = new Random();
-            return random.Next(min, max);
+            chars[i] = validChars[random.Next(0, validChars.Length)];
         }
-
-        public static string CreateAlphanumericCode(int length = 14)
-        {
-
-            var validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
-            var random = new Random();
-
-            var chars = new char[length];
-            for (var i = 0; i < length; i++)
-            {
-                chars[i] = validChars[random.Next(0, validChars.Length)];
-            }
-            return new string(chars);
-        }
+        return new string(chars);
     }
 }
