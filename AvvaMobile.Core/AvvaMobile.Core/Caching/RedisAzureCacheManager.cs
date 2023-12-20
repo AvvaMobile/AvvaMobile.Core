77c424cd7fb891aa72df8d611d97e3746854ea5c
dogukanergezer <dogukanergezer@users.noreply.github.com>
2023-12-20 15:38:59 +0300

    new redis clien added

---
+++ AvvaMobile.Core/AvvaMobile.Core/Caching/RedisAzureCacheManager.cs
@@ -0,0 +1,84 @@
+﻿using AvvaMobile.Core.Redis;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
+
+namespace AvvaMobile.Core.Caching;
+
+public class RedisAzureCacheManager : ICacheManager
+{
+    private readonly IRedisAzureClient _cache;
+
+    public RedisAzureCacheManager(IRedisAzureClient cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<bool> IsExists(string key)
+    {
+        return await _cache.IsExists(key);
+    }
+
+    public async Task<T> Get<T>(string key)
+    {
+        return await _cache.Get_Deserialized<T>(key);
+    }
+
+    public async Task<string> Get(string key)
+    {
+        return await _cache.Get_String(key);
+    }
+
+    public async Task<bool> Set(string key, object value)
+    {
+        return value switch
+        {
+            string => await _cache.Set(key, value.ToString()),
+            int or long or decimal => await _cache.Set(key, value.ToString()),
+            _ => await _cache.Set(key, JsonSerializer.Serialize(value))
+        };
+    }
+
+    public async Task<bool> Set(string key, object value, TimeSpan expiry)
+    {
+        return await _cache.Set(key, value.ToString(), expiry);
+    }
+
+    public async Task<List<SelectListItem>> Get_SelectListItems(string key)
+    {
+        return await _cache.Get_SelectListItems(key);
+    }
+
+    public async Task<bool> Set_SelectListItems(string key, List<SelectListItem> value)
+    {
+        return await _cache.Set_SelectListItems(key, value);
+    }
+
+    public async Task Remove(string key)
+    {
+        await _cache.Remove(key);
+    }
+
+    public Task Clear()
+    {
+        _cache.ClearAll();
+        return Task.CompletedTask;
+    }
+
+    [Obsolete("This method is obsolete. Use Set method with timespan parameters instead.")]
+    public void SetHours(string key, object data, int hours)
+    {
+        throw new Exception("This method is obsolete. Use Set method without timespan parameters instead.");
+    }
+
+    [Obsolete("This method is obsolete. Use Set method with timespan parameters instead.")]
+    public void SetMinutes(string key, object data, int minutes)
+    {
+        throw new Exception("This method is obsolete. Use Set method without timespan parameters instead.");
+    }
+
+    [Obsolete("This method is obsolete. Use Set method without timespan parameters instead.")]
+    public void SetNeverRemove(string key, object data)
+    {
+        throw new Exception("This method is obsolete. Use Set method without timespan parameters instead.");
+    }
+}
